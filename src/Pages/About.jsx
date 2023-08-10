7e8295120268b89dd94afd700135cd7ddfd7fe46
ibn <officialibn001@gmail.com>
2023-08-10 23:28:11 +0100

    About page complete

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,12 @@
 import '../components/styles/About.css'
 import AboutBanner from "../components/AboutBanner";
+import AboutText from '../components/AboutText';
 
 const About = () => {
     return (
         <div>
             <AboutBanner />
-            
+            <AboutText />
         </div>
     );
 }
---
+++ src/components/AboutText.jsx
@@ -0,0 +1,65 @@
+
+const AboutText = () => {
+    return (
+        <div className="aboutText">
+            <p>
+                At TechMania, we&apos;re passionate about technology and how it can transform the way we live, work, and interact. Our mission is to provide you with the latest and greatest tech products, keeping you at the forefront of innovation.
+            </p>
+
+            <p>                
+                Our Story:
+                TechMania started as a dream of creating a hub for tech enthusiasts, a place where you can explore the most cutting-edge gadgets, stay updated on industry trends, and find the perfect tech solutions for your needs. We believe that technology should be accessible to everyone, and our platform is designed to make that possible.
+            </p>
+
+            <h3>
+                What We Offer:
+            </h3>
+            <ul>
+                <li>
+                    - Curated Selection: We carefully curate a diverse range of tech products, from smartphones and laptops to smart home devices and accessories. Each product on our platform is chosen for its quality, innovation, and value.
+                </li>
+
+                <li>
+                    - Expert Advice: Our team of tech experts is here to guide you through your tech journey. Whether you&apos;re a tech novice or a seasoned pro, we&apos;re here to provide you with the information you need to make informed decisions.
+                </li>
+
+                <li>
+                    - Seamless Experience: We believe that exploring and purchasing tech products should be easy and enjoyable. Our user-friendly website is designed to provide you with a seamless shopping experience, and our customer support team is always ready to assist you.
+                </li>
+
+                <li>
+                    - Tech Community: Join our vibrant tech community where you can connect with fellow tech enthusiasts, share your experiences, and discover new and exciting tech trends.
+                </li>
+            </ul>
+
+            <h3>
+                Why Choose TechMania:
+            </h3>
+
+            <ul>
+                <li>
+                    - Quality Assurance: We stand by the quality of our products. Each item is carefully inspected and tested to ensure that it meets our high standards.
+                </li>
+
+                <li>
+                    - Innovation: We&apos;re committed to staying at the forefront of tech innovation. We&apos;re always on the lookout for the latest tech breakthroughs and strive to bring them to you.
+                </li>
+
+                <li>
+                    - Customer-Centric: Your satisfaction is our priority. We&apos;re dedicated to providing excellent customer service, and we&apos;re here to support you every step of the way.
+                </li>
+            </ul>
+
+            <p>
+                Thank you for choosing TechMania as your go-to destination for all things tech. We&apos;re excited to be part of your tech journey and can&apos;t wait to see where technology takes us next.<br/><br />
+            
+                Stay tuned for the latest updates, exclusive offers, and exciting tech discoveries. Join us in embracing the world of tech at TechMania!
+
+                <br /><br />
+                The TechMania Team 🖤
+            </p>
+        </div>
+    );
+}
+
+export default AboutText;
