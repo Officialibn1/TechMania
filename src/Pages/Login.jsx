3e2fb364b651dd3296eb02a6a374abcf1e57646e
ibn <officialibn001@gmail.com>
2023-08-16 10:56:13 +0100

    Login components created

---
+++ src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+import '../components/styles/Login.css'
+
+const Login = () => {
+    return (
+        <div className='loginContainer'>
+            <div className="login">
+
+            </div>
+        </div>
+    );
+}
+
+export default Login;
